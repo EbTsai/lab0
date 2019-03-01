800fe3f90e8f2f072f81073c0e51a9da85bc8287
EbTsai <einbeck201@icloud.com>
2019-03-01 22:25:01 +0800

    Add fields to point to the tail

--- queue.h
+++ queue.h
@@ -25,11 +25,13 @@ typedef struct ELE {
 
 /* Queue structure */
 typedef struct {
-    list_ele_t *head; /* Linked list of elements */
-                      /*
-                        You will need to add more fields to this structure
-                        to efficiently implement q_size and q_insert_tail
-                      */
+    list_ele_t *head;          /* Linked list of elements */
+                               /*
+                                 You will need to add more fields to this structure
+                                 to efficiently implement q_size and q_insert_tail
+                               */
+    list_ele_t *q_insert_tail; /* Tail of the linked list */
+    size_t q_size;
 } queue_t;
 
 /************** Operations on queue ************************/
